1dc0a96fc45091773b3695109f35e259bc5a592c
Reidy <85745107+This-is-not-Reidy@users.noreply.github.com>
2021-10-17 22:24:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+MIT License
+Copyright (c) 2021 Devon (Gorialis) R
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import os
 import pathlib
 import re
@@ -5,11 +28,17 @@ import subprocess
 
 from setuptools import setup
 
-
 ROOT = pathlib.Path(__file__).parent
 
-EXTRA_REQUIRES = {}
+with open(ROOT / 'jishaku_nextcord' / 'meta.py', 'r', encoding='utf-8') as f:
+    VERSION_MATCH = re.search(r'VersionInfo\(major=(\d+), minor=(\d+), micro=(\d+), .+\)', f.read(), re.MULTILINE)
 
+    if not VERSION_MATCH:
+        raise RuntimeError('version is not set or could not be located')
+
+    VERSION = '.'.join([VERSION_MATCH.group(1), VERSION_MATCH.group(2), VERSION_MATCH.group(3)])
+
+EXTRA_REQUIRES = {}
 
 for feature in (ROOT / 'requirements').glob('*.txt'):
     with open(feature, 'r', encoding='utf-8') as f:
@@ -17,29 +46,66 @@ for feature in (ROOT / 'requirements').glob('*.txt'):
 
 REQUIREMENTS = EXTRA_REQUIRES.pop('_')
 
-version = ''
-with open('jishaku_nextcord/meta.py') as f:
-    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
-
-if not version:
+if not VERSION:
     raise RuntimeError('version is not set')
 
-readme = ''
-with open('README.md') as f:
-    readme = f.read()
 
-setup(name='jishaku_nextcord',
-      author='This is not Reidy',
-      url='https://github.com/This-is-not-Reidy/jishaku-nextcord.git',
-      project_urls={
-        "Author Discord": "https://discord.com/users/848593011038224405",
-      },
-      version=version,
-      packages=['jishaku_nextcord'],
-      description='This is a fork of jishaku for nextcord',
-      long_description=readme,
-      long_description_content_type='text/markdown',
-      include_package_data=True,
-      install_requires=REQUIREMENTS,
-      extras_require=EXTRA_REQUIRES
-      )
+try:
+    PROCESS = subprocess.Popen(
+        ['git', 'rev-list', '--count', 'HEAD'],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+
+    COMMIT_COUNT, ERR = PROCESS.communicate()
+
+    if COMMIT_COUNT:
+        PROCESS = subprocess.Popen(
+            ['git', 'rev-parse', '--short', 'HEAD'],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+
+        COMMIT_HASH, ERR = PROCESS.communicate()
+
+        if COMMIT_HASH:
+            match = re.match(r'(\d).(\d).(\d)(a|b|rc)?', os.getenv('tag_name') or "")
+
+            if (match and match[4]) or not match:
+                VERSION += ('' if match else 'a') + COMMIT_COUNT.decode('utf-8').strip() + '+g' + COMMIT_HASH.decode('utf-8').strip()
+
+                # Also attempt to retrieve a branch, when applicable
+                PROCESS = subprocess.Popen(
+                    ['git', 'symbolic-ref', '-q', '--short', 'HEAD'],
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE
+                )
+
+                COMMIT_BRANCH, ERR = PROCESS.communicate()
+
+                if COMMIT_BRANCH:
+                    VERSION += "." + re.sub('[^a-zA-Z0-9.]', '.', COMMIT_BRANCH.decode('utf-8').strip())
+
+except FileNotFoundError:
+    pass
+
+
+with open(ROOT / 'README.md', 'r', encoding='utf-8') as f:
+    README = f.read()
+
+
+setup(
+    name='jishaku_nextcord',
+    author='This is not Reidy',
+    url='https://github.com/This-is-not-Reidy/jishaku-nextcord.git',
+    description='This is a fork of jishaku for nextcord',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    version=VERSION,
+    packages=['jishaku_nextcord', 'jishaku_nextcord.features', 'jishaku_nextcord.repl', 'jishaku_nextcord.shim'],
+    include_package_data=True,
+    install_requires=REQUIREMENTS,
+    python_requires='>=3.8.0',
+
+    extras_require=EXTRA_REQUIRES
+)
