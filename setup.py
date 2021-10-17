7e451893b5b15dcbf2e281adfd4bbadc8c2481cd
Reidy <85745107+This-is-not-Reidy@users.noreply.github.com>
2021-10-18 02:54:31 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,19 +3,15 @@
 
 """
 MIT License
-
 Copyright (c) 2021 Devon (Gorialis) R
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -34,7 +30,7 @@ from setuptools import setup
 
 ROOT = pathlib.Path(__file__).parent
 
-with open(ROOT / 'jishaku' / 'meta.py', 'r', encoding='utf-8') as f:
+with open(ROOT / 'jishaku_nextcord' / 'meta.py', 'r', encoding='utf-8') as f:
     VERSION_MATCH = re.search(r'VersionInfo\(major=(\d+), minor=(\d+), micro=(\d+), .+\)', f.read(), re.MULTILINE)
 
     if not VERSION_MATCH:
@@ -99,45 +95,17 @@ with open(ROOT / 'README.md', 'r', encoding='utf-8') as f:
 
 
 setup(
-    name='jishaku',
-    author='Devon (Gorialis) R',
-    url='https://github.com/Gorialis/jishaku',
-
-    license='MIT',
-    description='A discord.py extension including useful tools for bot development and debugging.',
+    name='jishaku_nextcord',
+    author='This is not Reidy',
+    url='https://github.com/This-is-not-Reidy/jishaku-nextcord.git',
+    description='This is a fork of jishaku for nextcord',
     long_description=README,
     long_description_content_type='text/markdown',
-    project_urls={
-        'Documentation': 'https://jishaku.readthedocs.io/en/latest/',
-        'Code': 'https://github.com/Gorialis/jishaku',
-        'Issue tracker': 'https://github.com/Gorialis/jishaku/issues'
-    },
-
     version=VERSION,
-    packages=['jishaku', 'jishaku.features', 'jishaku.repl', 'jishaku.shim'],
+    packages=['jishaku_nextcord', 'jishaku_nextcord.features', 'jishaku_nextcord.repl', 'jishaku_nextcord.shim'],
     include_package_data=True,
     install_requires=REQUIREMENTS,
     python_requires='>=3.8.0',
 
-    extras_require=EXTRA_REQUIRES,
-
-    download_url='https://github.com/Gorialis/jishaku/archive/{}.tar.gz'.format(VERSION),
-
-    keywords='jishaku discord.py discord cog repl extension',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Framework :: AsyncIO',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Topic :: Communications :: Chat',
-        'Topic :: Internet',
-        'Topic :: Software Development :: Debuggers',
-        'Topic :: Software Development :: Testing',
-        'Topic :: Utilities'
-    ]
+    extras_require=EXTRA_REQUIRES
 )
